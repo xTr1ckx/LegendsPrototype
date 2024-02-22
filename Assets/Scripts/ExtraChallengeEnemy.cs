1e36b056ab826066899e35d5129408581224a1cd
Edgars <62717850+xTr1ckx@users.noreply.github.com>
2024-02-22 15:39:57 +0200

    Commit #1

---
+++ Assets/Scripts/ExtraChallengeEnemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraChallengeEnemy : MonoBehaviour
+{
+    public Stats enemyStats;
+
+    [Tooltip("The transform to which the enemy will pace back and forth to.")]
+    public Transform[] patrolPoints;
+
+    private int currentPatrolPoint = 1;
+
+    /// <summary>
+    /// Contains tunable parameters to tweak the enemy's movement.
+    /// </summary>
+    [System.Serializable]
+    public struct Stats
+    {
+        [Header("Enemy Settings")]
+
+        [Tooltip("How fast the enemy moves.")]
+        public float speed;
+
+        [Tooltip("Whether the enemy should move or not")]
+        public bool move;
+    }
+
+    void Update()
+    {
+        
+        //if the enemy is allowed to move
+        if (enemyStats.move == true)
+        {
+            Vector3 moveToPoint = patrolPoints[currentPatrolPoint].position;
+            transform.position = Vector3.MoveTowards(transform.position, moveToPoint, enemyStats.speed * Time.deltaTime);
+           
+            if (Vector3.Distance(transform.position, moveToPoint) < 0.01f)
+            {
+                currentPatrolPoint++;
+                
+                if (currentPatrolPoint >= patrolPoints.Length)
+                {
+                    currentPatrolPoint = 0;
+                }
+            
+            }
+        }
+    }
+}
